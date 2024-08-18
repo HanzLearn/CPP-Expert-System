b70d8a46d42cc90a9083f277527bac0ee175fa6c
Ilhan Hashim <ilhanmachine@gmail.com>
2024-08-18 15:36:38 +0800

    From EditText branch

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(){
     std::cout << "Hello!" << std::endl;
     std::cout << "Hellow new friends" << std::endl;
     std::cout << "Test 2" << std::endl;
+    std::cout << "New World!" << std::endl;
     return 0;
 }
 
